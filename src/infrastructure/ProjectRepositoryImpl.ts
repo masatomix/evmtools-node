3db316bf7a54032d9f8005b97816ce5dae94ec66
Masatomi KINO <masatomix@ki-no.org>
2025-06-23 18:15:57 +0900

    WEBからつかえるようリファクタリング中 Repository切り出し

---
+++ src/infrastructure/ProjectRepositoryImpl.ts
@@ -0,0 +1,99 @@
+import { createWorkbook, json2workbook, toFileAsync } from 'excel-csv-read-write'
+import { dateStr } from '../common'
+import { createStyles } from '../common/styles'
+import { ProjectStatistics, AssigneeStatistics } from '../domain'
+import { ProjectRepository } from '../domain/ProjectRepository'
+
+export class ProjectRepositoryImpl implements ProjectRepository {
+    writeProjectInfo: (data: {
+        statisticsByProject?: ProjectStatistics[]
+        statisticsByName?: AssigneeStatistics[]
+        pvByProject?: Record<string, unknown>[]
+        pvsByProject?: Record<string, unknown>[]
+        pvByName?: Record<string, unknown>[]
+        pvsByName?: Record<string, unknown>[]
+        projectData?: Record<string, unknown>[]
+        baseDate: Date
+        path: string
+    }) => Promise<void> = async ({
+        statisticsByProject,
+        statisticsByName,
+        pvByProject,
+        pvsByProject,
+        pvByName,
+        pvsByName,
+        projectData,
+        baseDate,
+        path,
+    }) => {
+        const workbook = await createWorkbook()
+
+        const dateStrHyphen = dateStr(baseDate).replace(/\//g, '-')
+
+        if (statisticsByProject) {
+            console.log('プロジェクト情報')
+            console.table(statisticsByProject)
+            json2workbook({
+                instances: statisticsByProject,
+                workbook,
+                sheetName: `プロジェクト情報`,
+                applyStyles: createStyles(),
+            })
+        }
+        if (statisticsByName) {
+            console.log('要員ごと統計')
+            console.table(statisticsByName)
+            json2workbook({
+                instances: statisticsByName,
+                workbook,
+                sheetName: '要員ごと統計',
+                applyStyles: createStyles(),
+            })
+        }
+
+        if (pvByProject) {
+            json2workbook({
+                instances: pvByProject,
+                workbook,
+                sheetName: `プロジェクト日ごとPV`,
+                applyStyles: createStyles(),
+            })
+        }
+        if (pvsByProject) {
+            json2workbook({
+                instances: pvsByProject,
+                workbook,
+                sheetName: `プロジェクト日ごと累積PV`,
+                applyStyles: createStyles(),
+            })
+        }
+
+        if (pvByName) {
+            json2workbook({
+                instances: pvByName,
+                workbook,
+                sheetName: `要員ごと・日ごとPV`,
+                applyStyles: createStyles(),
+            })
+        }
+        if (pvsByName) {
+            json2workbook({
+                instances: pvsByName,
+                workbook,
+                sheetName: `要員ごと・日ごと累積PV`,
+                applyStyles: createStyles(),
+            })
+        }
+
+        if (projectData) {
+            json2workbook({
+                instances: projectData,
+                workbook,
+                sheetName: `素データ_${dateStrHyphen}`,
+                applyStyles: createStyles(),
+            })
+        }
+        workbook.deleteSheet('Sheet1')
+        await toFileAsync(workbook, path)
+    }
+}
